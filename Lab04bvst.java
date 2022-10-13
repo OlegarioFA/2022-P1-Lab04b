5b33d339bad3f5272c4e421568f83c8946d7f555
Olegario <olegario.fernandezalonso@vtsd.com>
2022-10-13 09:06:04 -0400

    code Update

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -13,10 +13,17 @@ public class Lab04bvst extends Applet
     public void paint(Graphics g)
     {
         // DRAW CUBE
-
+        g.drawRect(50,50,200,200 );
+        g.drawRect(150,150,200,200 );
+        g.drawLine(50,50, 150,150);
+        g.drawLine(250,250, 350,350);
+        g.drawLine(250,50, 350,150);
+        g.drawLine(50,250, 150,350);
 
 
         // DRAW SPHERE
+        g.drawOval(500,50,250,250);
+
 
 
 
@@ -25,6 +32,27 @@ public class Lab04bvst extends Applet
 
 
         // DRAW APCS
+        g.fillRect(50,400,25,125);
+        g.fillRect(100,400,25,125);
+        g.fillRect(50,400,50,25);
+        g.fillRect(50,450,50,25);
+
+        g.fillRect(150,400,25,125);
+        g.fillRect(170,400,50,25);
+        g.fillRect(200,400,25,75);
+        g.fillRect(170,450,50,25);
+
+        g.fillRect(250,400,25,125);
+        g.fillRect(250,400,75,25);
+        g.fillRect(250,500,75,25);
+
+        g.fillRect(350,450,75,25);
+        g.fillRect(350,400,75,25);
+        g.fillRect(350,400,25,75);
+        g.fillRect(350,500,75,25);
+        g.fillRect(400,450,25,75);
+
+
 
 
 
